6e45d7a04b6cbb6ce407c8b66a145b8a4d332d33
Adam <benbourhim7@gmail.com>
2021-02-07 16:36:19 +0200

    section2

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -39,10 +39,12 @@ function Main() {
             Dwidasa Samsara Indonesia (DSI) was firstly established in 2010 by
             the founders, who each of them has a common end objective to
             innovate new creations by making the most of the ever-growing
-            information technology through DSI’s distinct front-end based
-            application concept.
+            information technology through{" "}
+            <span style={{ color: "#EE4248" }}>DSI</span>’s distinct front-end
+            based application concept.
             <br /> Managed by a team of professional experts with extensive
-            experience and impressive track records, DSI believes that
+            experience and impressive track records,{" "}
+            <span style={{ color: "#EE4248" }}>DSI</span> believes that
             continuous improvements and innovations assure your business to run
             effectively and efficiently.
           </p>
