bb5a548b8cc950aa22495d58e2102078ba418381
Adam <benbourhim7@gmail.com>
2021-02-09 16:32:34 +0200

    responsible

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./Header/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Main from "./Main/Main";
+
 function App() {
   return (
     <div>
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,55 +1,57 @@
 import React from "react";
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+
 import logo from "../Images/DSI.svg";
 import styles from "./navbar.module.css";
 import union from "../Images/Union.svg";
 import mail from "../Images/mail.svg";
 import media from "../Images/shuffle.svg";
 function Header() {
+  const toggleButton = document.getElementsByClassName("button")[0];
+  const navbarLinks = document.getElementsByClassName("links")[0];
+  const show = () => {
+    toggleButton.addEventListener("click", () => {
+      navbarLinks.classList.toggle("active");
+    });
+  };
+
   return (
-    <Navbar
-      collapseOnSelect
-      expand="lg"
-      bg="dark"
-      text="dark"
-      className={styles.navHead}
-    >
-      <div className={styles.logo}>
-        <Navbar.Brand href="#home">
-          <img src={logo} />
-        </Navbar.Brand>
+    <nav className={styles.navbar}>
+      <div className={styles.title}>
+        <img src={logo} alt="" />
       </div>
-      <div className={styles.list}>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
+      <a href="#" className={styles.button} onClick={show}>
+        <span className={styles.bar}></span>
+        <span className={styles.bar}></span>
+        <span className={styles.bar}></span>
+      </a>
+      <div className={styles.links}>
+        <ul>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">Product</a>
+          </li>
+          <li>
+            <a href="#">Technology</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Client</a>
+          </li>
+          <li>
+            <a href="#">Partner</a>
+          </li>
+        </ul>
       </div>
       <div className={styles.icon}>
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        <img src={union} alt="" />
+        <img src={mail} alt="" />
+        <img src={media} alt="" />
       </div>
-    </Navbar>
+    </nav>
   );
 }
 
