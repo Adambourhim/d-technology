0bdceda01cc586c3f10b868e538468fa151073f4
Adam <benbourhim7@gmail.com>
2021-02-23 12:32:37 +0200

    navbar

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,52 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../HomePage/Images/DSI.svg";
 import styles from "./navbar.module.css";
 import union from "../HomePage/Images/Union.svg";
 import mail from "../HomePage/Images/mail.svg";
 import media from "../HomePage/Images/shuffle.svg";
-import edit from "../HomePage/Images/edit.svg";
+import { GrClose } from "react-icons/gr";
+import { GrTextAlignRight } from "react-icons/gr";
+
 function Header() {
+  const [showbar, setShowbar] = useState(false);
   return (
-    <div className={styles.header}>
-      <nav className={styles.navbar}>
-        <div className={styles.title}>
-          <Link to="/">
-            <img src={logo} alt="" />
-          </Link>
+    <>
+      <div className={styles.header}>
+        <nav className={styles.navbar}>
+          <div className={styles.title}>
+            <Link to="/">
+              <img src={logo} alt="" />
+            </Link>
+          </div>
+          <div
+            href="#"
+            className={styles.button}
+            onClick={() => setShowbar(!showbar)}
+          >
+            {showbar ? (
+              <GrClose size="30px" />
+            ) : (
+              <GrTextAlignRight size="30px" />
+            )}
+          </div>
+          <div className={styles.links}>
+            <ul>
+              <li>
+                <Link to="/Service">Services</Link>
+              </li>
+              <li>
+                <Link to="/Product">Product</Link>
+              </li>
+              <li>
+                <Link to="/Technology">Technology</Link>
+              </li>
+              <li>
+                <Link to="/About">About</Link>
+              </li>
+              <li>
+                <a href="#">Client</a>
+              </li>
+              <li>
+                <a href="#">Partner</a>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.icon}>
+            <img src={union} alt="" />
+            <img src={mail} alt="" />
+            <img src={media} alt="" />
+          </div>
+        </nav>
+      </div>
+
+      {showbar ? (
+        <div className={styles.navbar2}>
+          <div className={styles.links2}>
+            <ul>
+              <li>
+                <Link to="/Service" onClick={() => setShowbar(!showbar)}>
+                  Services
+                </Link>
+              </li>
+              <li>
+                <Link to="/Product" onClick={() => setShowbar(!showbar)}>
+                  Product
+                </Link>
+              </li>
+              <li>
+                <Link to="/Technology" onClick={() => setShowbar(!showbar)}>
+                  Technology
+                </Link>
+              </li>
+              <li>
+                <Link to="/About" onClick={() => setShowbar(!showbar)}>
+                  About
+                </Link>
+              </li>
+              <li>
+                <a href="#">Client</a>
+              </li>
+              <li>
+                <a href="#">Partner</a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <a href="#" className={styles.button}>
-          <img src={edit} alt="" />
-        </a>
-        <div className={styles.links}>
-          <ul>
-            <li>
-              <Link to="/Service">Services</Link>
-            </li>
-            <li>
-              <Link to="/Product">Product</Link>
-            </li>
-            <li>
-              <Link to="/Technology">Technology</Link>
-            </li>
-            <li>
-              <Link to="/About">About</Link>
-            </li>
-            <li>
-              <a href="#">Client</a>
-            </li>
-            <li>
-              <a href="#">Partner</a>
-            </li>
-          </ul>
-        </div>
-        <div className={styles.icon}>
-          <img src={union} alt="" />
-          <img src={mail} alt="" />
-          <img src={media} alt="" />
-        </div>
-      </nav>
-    </div>
+      ) : null}
+    </>
   );
 }
 
